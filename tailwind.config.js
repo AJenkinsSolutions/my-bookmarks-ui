c3bf3d854766c0b54c59fd4ab5da6b20606e76a9
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-05-01 11:41:59 -0500

    setup and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,18 @@ module.exports = {
 
     },
     extend: {
-      fontFamily: {
-
-      },
       colors: {
-        
-      }
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
+        veryDarkBlue: 'hsl(229, 31%, 21%)',
+      },
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif'],
+      },
+      backgroundImage: () => ({
+        dots: "url('../images/bg-dots.svg')",
+      })
     },
   },
   plugins: [],
