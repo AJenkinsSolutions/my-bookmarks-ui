b4f1a990b26550b8658b6c8bddcb17c12f7d02fb
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-05-02 09:20:48 -0500

    Feat: Add tabs and panel section

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// Dynamiclly render our panel content when the tabs are selected
+const tabs = document.querySelectorAll('.tab');
+const panels = document.querySelectorAll('.panel');
+
+
+// Tabs Event Listerner
+tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+
+
+function onTabClick(e){
+    //e .target refers to the the tab being clicked
+    
+    // Deactovate all tabs
+    tabs.forEach((tab)=>{
+        //console.log(tab.children[0])
+        tab.children[0].classList.remove(
+            'border-softRed',
+            'border-b-4',
+            'border-b-0'
+        )
+    })
+    // Hide all panels aka add hidden class
+    panels.forEach((panel) => {
+        panel.classList.add('hidden')
+    })
+
+    // Activate a new tab & panel, based on target 
+    e.target.classList.add('border-softRed', 'border-b-4')
+    const classString = e.target.getAttribute('data-target')
+    //console.log('Debug: ClassString: ' + classString)
+    
+    // Gets the panels -section by Id
+    // Get the specific panel by class name from the data target (panel-1 or panel-2 etc...)
+    // Remove the hidden class from that panel so its visable
+    document
+    .getElementById('panels-section')
+    .getElementsByClassName(classString)[0]
+    .classList.remove('hidden')
+
+
+
+    console.log("Debug: target "+ e.target.classList)
+
+}
